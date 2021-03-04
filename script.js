4ab9100d80880152f246a726ae6a38904acf6780
Lexan E Flowers <lexar5657@gmail.com>
2021-03-04 16:51:33 -0600

    updates

---
+++ script.js
@@ -0,0 +1,12 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+console.log(screen.width);
+
+document.addEventListener("scroll", function() {
+    let head = document.querySelector(".head");
+    if (head.offsetTop > 54) {
+        let max = (screen.width - 20) + "px";
+        head.style.width = max;
+    } else
+        head.style.width = "350px";
+
+});
